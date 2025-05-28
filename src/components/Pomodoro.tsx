533167be7627da121dfa1122e4029f725e05eda4
sfaure <sebastian.faure.l@gmail.com>
2025-05-28 16:12:00 -0400

    [add] components to pomodoro, move to botom

--- src/components/Pomodoro.tsx
+++ src/components/Pomodoro.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect, useRef  } from 'react';
+import { useState, useEffect, useRef } from 'react';
 
 import Countdown from './Countdown.tsx';
 import { Toaster, toast } from 'sonner'
@@ -16,8 +16,8 @@ export default function Pomodoro() {
     audioRef.current = new Audio(bell);
   }, []);
 
-  const toggle 
-  = () => setPaused((prev) => !prev);
+  const toggle
+    = () => setPaused((prev) => !prev);
 
   function MyIcon() {
     return (
@@ -32,7 +32,7 @@ export default function Pomodoro() {
     );
   }
 
-  function onCountEnd(){
+  function onCountEnd() {
     audioRef.current?.play();
     toast('Focus time ended, register or discard?', {
       duration: Infinity,
@@ -43,15 +43,15 @@ export default function Pomodoro() {
       },
       cancel: {
         label: 'Cancel',
-        onClick: () => {}
+        onClick: () => { }
       },
       icon: <MyIcon />
     })
   }
 
-  async function updateTask(){
+  async function updateTask() {
     let taskId = selectedTask?.id;
-    if(taskId){
+    if (taskId) {
       const response = await fetch(`/api/boards/123/tasks/${taskId}/focus_sessions`, {
         method: 'POST',
         headers: {
@@ -63,7 +63,7 @@ export default function Pomodoro() {
           user: 'juan.perez'
         }),
       });
-  
+
       if (response.ok) {
         alert('Focus session logged successfully!');
       } else {
@@ -71,60 +71,103 @@ export default function Pomodoro() {
         console.error(await response.text());
       }
     }
-    else{
+    else {
       alert('No task selected');
     }
   }
 
   return (
     <section
-      className="flex flex-col rounded-md gap-2 bg-midnight-50 dark:bg-midnight-700 p-2"
-    > 
+      className=" max-h-40
+        fixed bottom-4 left-4 right-4 bg-white shadow-lg p-4 z-50
+        flex rounded-md items-center gap-2 bg-midnight-50 dark:bg-midnight-700 
+      "
+    >
       <Toaster />
-      <span
-        className="rounded-full max-w-fit px-4 bg-midnight-200 dark:bg-midnight-600 hover:bg-midnight-300 hover:dark:bg-midnight-500"
-      >
-        Focus
-      </span>
-      <div className="flex items-center">
-
-        <div className="relative">
-          <label
-            htmlFor="play-toggle"
-            className="flex items-center justify-center size-24 rounded-full bg-midnight-200 dark:bg-midnight-600 hover:bg-midnight-300 hover:dark:bg-midnight-500 cursor-pointer"
-          >
-            <input
-              type="checkbox"
-              id="play-toggle"
-              className="hidden peer"
-              checked={!paused}
-              onChange={toggle}
-            />
-            <svg
-              className="w-10 h-10 text-white peer-checked:hidden"
-              xmlns="http://www.w3.org/2000/svg"
-              fill="currentColor"
-              viewBox="0 0 24 24"
-            >
-              <path d="M5 3v18l15-9L5 3z"></path>
-            </svg>
-            <svg
-              className="w-10 h-10 text-white hidden peer-checked:block"
-              xmlns="http://www.w3.org/2000/svg"
-              fill="currentColor"
-              viewBox="0 0 24 24"
-            >
-              <path d="M6 19h4V5H6v14zm8-14v14h4V5h-4z"></path>
-            </svg>
-          </label>
-
-        </div>
-          <Countdown
-            className="px-4 text-6xl font-semibold text-midnight-900 dark:text-white"
-            paused={paused}
-            durationMinutes={.5}
-            onFinish={onCountEnd}
+      {/* play button */}
+      <div className="relative">
+        <label
+          htmlFor="play-toggle"
+          className="size-24 flex items-center justify-center rounded-full bg-midnight-200 dark:bg-midnight-600 hover:bg-midnight-300 hover:dark:bg-midnight-500 cursor-pointer"
+        >
+          <input
+            type="checkbox"
+            id="play-toggle"
+            className="hidden peer"
+            checked={!paused}
+            onChange={toggle}
           />
+          <svg
+            className="w-10 h-10 text-white peer-checked:hidden"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="currentColor"
+            viewBox="0 0 24 24"
+          >
+            <path d="M5 3v18l15-9L5 3z"></path>
+          </svg>
+          <svg
+            className="w-10 h-10 text-white hidden peer-checked:block"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="currentColor"
+            viewBox="0 0 24 24"
+          >
+            <path d="M6 19h4V5H6v14zm8-14v14h4V5h-4z"></path>
+          </svg>
+        </label>
+
+      </div>
+      <div className="flex flex-col items-center w-full">
+        <p className="text-2xl">Current task</p>
+        <Countdown
+          className="px-4 text-6xl pb-2 font-semibold text-midnight-900 dark:text-white"
+          paused={paused}
+          durationMinutes={.5}
+          onFinish={onCountEnd}
+        />
+        <p><span
+          className="rounded-full max-w-fit px-2 pb-1 bg-midnight-200 dark:bg-midnight-600 hover:bg-midnight-300 hover:dark:bg-midnight-500"
+        >
+          Focus
+        </span> time
+        </p>
+      </div>
+      <div className="flex flex-col max-w-28 w-full space-y-4">
+        <button
+          className="
+              flex items-center justify-center w-full pb-1 
+              rounded-md cursor-pointer text-white transition
+              bg-midnight-200  hover:bg-midnight-300
+              dark:bg-midnight-600 hover:dark:bg-midnight-500 
+            "
+          title="Reset timer"
+        // onClick={handleReset}
+        >
+          Edit task
+        </button>
+        <button
+          className="
+              flex items-center justify-center w-full pb-1 
+              rounded-md cursor-pointer text-white transition
+              bg-midnight-200  hover:bg-midnight-300
+              dark:bg-midnight-600 hover:dark:bg-midnight-500 
+            "
+          title="Reset timer"
+        // onClick={handleReset}
+        >
+          Reset Timer
+        </button>
+        <button
+          className="
+              flex items-center justify-center w-full pb-1
+              rounded-md cursor-pointer text-white transition
+              bg-midnight-200  hover:bg-midnight-300
+              dark:bg-midnight-600 hover:dark:bg-midnight-500 
+            "
+          title="Reset timer"
+        // onClick={handleReset}
+        >
+          Finish Task
+        </button>
       </div>
     </section>
   );
